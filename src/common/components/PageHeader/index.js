4d78e28c4b11dc4517ca32206f41015f36e43b29
Imad Boumzaoued <riseup.dev@gmail.com>
2019-01-08 17:55:46 +0100

    dashboard changes (#1)

--- src/common/components/PageHeader/index.js
+++ src/common/components/PageHeader/index.js
@@ -1,1 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Flex, Box } from "../Grid";
+import Icon from "../Icon";
+
+const PageHeader = props => {
+  return (
+    <Flex>
+      <Box>{props.tabName}</Box>
+      <Box>
+        <div className="crumbs">
+          <Icon icon={"home"} size={32} />
+        </div>
+      </Box>
+      <Box>actions</Box>
+    </Flex>
+  );
+};
+
+PageHeader.propTypes = {
+  tabName: PropTypes.string,
+  type: PropTypes.oneOf("tabs", "sideRight", "sideLeft"),
+  actions: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default PageHeader;
