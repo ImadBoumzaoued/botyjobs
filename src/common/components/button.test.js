40dd5a544815b7ad5d8232723bf623ff24d2ed82
imadboumzaoued <riseup.dev@gmail.com>
2019-12-03 16:18:51 +0100

    disabled test

--- src/common/components/button.test.js
+++ src/common/components/button.test.js
@@ -6,14 +6,28 @@ describe("<Button />", () => {
   afterEach(() => {
     cleanup();
   });
+
   it("renders without crashing", () => {
     render(<Button />);
   });
+
   it("calls onClick when clicked", () => {
     const onClick = jest.fn();
     const { getByText } = render(<Button onClick={onClick}>Click me</Button>);
 
     fireEvent.click(getByText("Click me"));
     expect(onClick).toBeCalledTimes(1);
+  });
+
+  it("doesn't fire onClick for disabled button", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <Button disabled onClick={onClick}>
+        Click me
+      </Button>,
+    );
+
+    fireEvent.click(getByText("Click me"));
+    expect(onClick).toBeCalledTimes(0);
   });
 });
