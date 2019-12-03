eae8b43c080ec8ec26f4b3a3972f428dde518441
imadboumzaoued <riseup.dev@gmail.com>
2019-12-03 15:25:28 +0100

    simple test

---
+++ src/common/components/button.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import Button from "./Button";
+
+describe("<Button />", () => {
+  afterEach(() => {
+    cleanup();
+  });
+  it("renders without crashing", () => {
+    render(<Button />);
+  });
+  it("calls onClick when clicked", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<Button onClick={onClick}>Click me</Button>);
+
+    fireEvent.click(getByText("Click me"));
+    expect(onClick).toBeCalledTimes(1);
+  });
+});
